0eaa46e67f261d9e03cc41dd7474dacbb618bf1f
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-03-06 20:28:09 -0300

    Componente: Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,15 +10,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background-color: ${props => props.theme['gray-800']};
     color: ${props => props.theme['gray-100']};
-  }
-
-  :focus {
-    outline: 0;
-    box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
     -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
     font: 400 1rem 'Roboto', sans-serif;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
   }
 `
