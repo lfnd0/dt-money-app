bfe0aa4f18bce7c9e834693f2afad8b133720194
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-03-06 17:55:41 -0300

    Componente: Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import logo from '../../assets/logo.svg';
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logo} alt="" />
+
+        <NewTransactionButton>
+          Nova transação
+        </NewTransactionButton>
+
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
